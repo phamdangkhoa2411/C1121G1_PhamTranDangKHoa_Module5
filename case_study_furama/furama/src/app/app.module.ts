949c4de39569aacf5873a1a04f00dc9895aa4884
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-05-10 18:55:47 +0700

    push

---
+++ case_study_furama/furama/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FuramaComponent } from './furama/furama.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ServiceComponent } from './service/service.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { EditCustomerComponent } from './edit-customer/edit-customer.component';
+import { ContractComponent } from './contract/contract.component';
+import { CreateContractComponent } from './create-contract/create-contract.component';
+import { VillaComponent } from './service-create/villa/villa.component';
+import { RoomComponent } from './service-create/room/room.component';
+import { HouseComponent } from './service-create/house/house.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FuramaComponent,
+    CustomerComponent,
+    ServiceComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateCustomerComponent,
+    EditCustomerComponent,
+    ContractComponent,
+    CreateContractComponent,
+    VillaComponent,
+    RoomComponent,
+    HouseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
