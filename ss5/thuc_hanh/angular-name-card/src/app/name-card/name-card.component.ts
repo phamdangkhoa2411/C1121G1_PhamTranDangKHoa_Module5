ea34f5fe3c321d400cc9908f3f79dfba0eebed23
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-05-10 15:10:05 +0700

    push

---
+++ ss5/thuc_hanh/angular-name-card/src/app/name-card/name-card.component.ts
@@ -0,0 +1,19 @@
+import { Component,Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-name-card',
+  templateUrl: './name-card.component.html',
+  styleUrls: ['./name-card.component.css']
+})
+export class NameCardComponent implements OnInit {
+
+  @Input() cardName: String ;
+  @Input() email: String ;
+  @Input() phone: String ;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
