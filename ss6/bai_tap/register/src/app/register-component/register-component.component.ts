ba01cb03747adc7e87e5f9d56b3896b7839215a3
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-05-11 20:26:01 +0700

    push

---
+++ ss6/bai_tap/register/src/app/register-component/register-component.component.ts
@@ -0,0 +1,79 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+
+
+@Component({
+  selector: 'app-register-component',
+  templateUrl: './register-component.component.html',
+  styleUrls: ['./register-component.component.css']
+})
+export class RegisterComponentComponent implements OnInit {
+  // email, password, confirmPassword, country, age, gender, phone
+
+  registerForm = new FormGroup({
+    email: new FormControl('',[Validators.email,Validators.required]),
+    passWord: new FormControl('' , [Validators.required,Validators.minLength(6),Validators.maxLength(12)]),
+    confirmPassword: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(12)]),
+    country: new FormControl('',[Validators.required]),
+    age: new FormControl('',[Validators.required,Validators.min(18),Validators.max(90)]),
+    gender: new FormControl('',[Validators.required]),
+    phone: new FormControl('',[Validators.required])
+  });
+
+  get email() {
+    return this.registerForm.get('email')
+  }
+
+  get passWord() {
+    return this.registerForm.get('passWord')
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword')
+  }
+
+  get country() {
+    return this.registerForm.get('country')
+  }
+  get age(){
+    return this.registerForm.get('age')
+  }
+  get gender(){
+    return this.registerForm.get('gender')
+  }
+  get phone(){
+    return this.registerForm.get('phone')
+  }
+
+
+  countryList  = [
+    {
+     id : 1 ,
+     name : 'đà nẵng'
+    },
+    {
+      id : 2 ,
+      name : 'đà lạt'
+    },
+    {
+      id : 3,
+      name : 'sài gòn'
+    }
+  ];
+
+
+
+
+
+  constructor(  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  onSubmit() {
+
+  }
+}
---
+++ ss6/bai_tap/register/src/app/register.ts
@@ -0,0 +1,11 @@
+export interface Register {
+  // email, password, confirmPassword, country, age, gender, phone
+  email?: number;
+  password?: string;
+  confirmPassword?: string;
+  country?: string;
+  age?: number;
+  gender?: string;
+  phone?: string;
+
+}
