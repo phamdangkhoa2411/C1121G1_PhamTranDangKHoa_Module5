fad9409982357ad81bb443daaf8f8370b83af164
phamdangkhoa2411 <jekhoa633@gmail.com>
2022-05-12 12:17:57 +0700

    push

--- ss6/bai_tap/register/src/app/register-component/register-component.component.ts
+++ ss6/bai_tap/register/src/app/register-component/register-component.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -12,18 +11,36 @@ export class RegisterComponentComponent implements OnInit {
   // email, password, confirmPassword, country, age, gender, phone
 
   registerForm = new FormGroup({
-    email: new FormControl('',[Validators.email,Validators.required]),
-    passWord: new FormControl('' , [Validators.required,Validators.minLength(6),Validators.maxLength(12)]),
-    confirmPassword: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(12)]),
-    country: new FormControl('',[Validators.required]),
-    age: new FormControl('',[Validators.required,Validators.min(18),Validators.max(90)]),
-    gender: new FormControl('',[Validators.required]),
-    phone: new FormControl('',[Validators.required,Validators.pattern("((09|03|07|08|05)+([0-9]{8})\\b)")])
+    email: new FormControl('', [Validators.email, Validators.required]),
+
+    // sử dụng custom validate
+    passWord: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+
+
+    country: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required, Validators.min(18), Validators.max(90)]),
+    gender: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required, Validators.pattern("((09|03|07|08|05)+([0-9]{8})\\b)")])
   });
+
+  // sử dụng custom validate
+  checkPass() {
+  let pw  = this.registerForm.get('passWord').value;
+  let confirmPass = this.registerForm.get('confirmPassword').value;
+  if (pw !== confirmPass){
+    this.registerForm.get('confirmPassword').setErrors({'check' :true}) ;
+  }
+
+ 
+  }
+
 
   get email() {
     return this.registerForm.get('email')
   }
+
+
 
   get passWord() {
     return this.registerForm.get('passWord')
@@ -36,37 +53,37 @@ export class RegisterComponentComponent implements OnInit {
   get country() {
     return this.registerForm.get('country')
   }
-  get age(){
+
+  get age() {
     return this.registerForm.get('age')
   }
-  get gender(){
+
+  get gender() {
     return this.registerForm.get('gender')
   }
-  get phone(){
+
+  get phone() {
     return this.registerForm.get('phone')
   }
 
 
-  countryList  = [
+  countryList = [
     {
-     id : 1 ,
-     name : 'đà nẵng'
+      id: 1,
+      name: 'đà nẵng'
     },
     {
-      id : 2 ,
-      name : 'đà lạt'
+      id: 2,
+      name: 'đà lạt'
     },
     {
-      id : 3,
-      name : 'sài gòn'
+      id: 3,
+      name: 'sài gòn'
     }
   ];
 
 
-
-
-
-  constructor(  ) {
+  constructor() {
   }
 
   ngOnInit(): void {
